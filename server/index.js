2ea5bef3cbe592ab748ff5e3ebb59e466c2b0608
Efrem Berezin <efrem430@gmail.com>
2024-06-19 18:37:25 -0500

    server created

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from "mongoose"
 import cors from "cors"
+import morgan from 'morgan';
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managmentRoutes from './routes/managment.js'
+import salesRoutes from './routes/sales.js'
 
 dotenv.config();
 const app = express();
@@ -13,8 +17,21 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin"}));
 app.use(morgan("common"))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ exteended:false}));
+app.use(bodyParser.urlencoded({ extended:false}));
 app.use(cors());
 
-app.use("/client", client)
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/managment", managmentRoutes);
+app.use("/sales", salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL,)
+    .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
+
 

a690de62d6936da84b3528566a9950159210d643
Efrem Berezin <efrem430@gmail.com>
2024-06-20 19:36:14 -0500

    dark mode/theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { CssBaseline, Cssbaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material"
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
   );
 }
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,19 +1,20 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit"
 
-const initialState = {
-    mode: "dark"
+const initialState = { 
+    mode : "dark"
 };
 
-export const globalSlice = createSlice({
+export const globalSlice = createSlice({ 
     name: "global",
     initialState,
     reducers: {
-        setMode : (state) => {
-            state.mode = state.mode === 'light' ? "dark" : 'light';
+        setMode: (state) => {
+            state.mode = state.mode === 'light' ? 'dark' : 'light';
         }
     }
 })
 
 export const { setMode } = globalSlice.actions;
 
-export default globalSlice.reducer;
+
+export default globalSlice.reducer

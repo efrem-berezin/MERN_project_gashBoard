86a6dddbb7a4859dabd895e3211e9d927f2db91f
Efrem Berezin <efrem430@gmail.com>
2024-06-26 18:40:38 -0500

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import { CssBaseline, Cssbaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material"
-import { themeSettings } from "theme";
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme";
 import { useSelector } from "react-redux"
 import { useMemo } from "react"
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+import Layout from "./scenes/layout/index";
+import Dashboard from "./scenes/dashboard/index";
 
 function App() {
-  const mode = useSelector((state) => state.global.mode);
+  const mode = useSelector((state) => state.global.mode)
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route element={<Layout/>}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />}/>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { configureStore } from "@reduxjs/toolkit"
-import globalReducer from "state";
-import { Provider } from "react-redux";
+
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from "./state/index"
+import { Provider } from "react-redux"
+
+// import { configureStore } from "@reduxjs/toolkit"
+// import globalReducer from "state";
+// import { Provider } from "react-redux";
+
+// const store = configureStore({
+//   reducer: { 
+//     global: globalReducer,
+//   }
+// })
 
 const store = configureStore({
-  reducer: { 
-    global: globalReducer,
-  }
+    reducer: {
+      global: globalReducer,
+    }
 })
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

7ccb16e9be1c2deb2152c8610cc5cfed861850ef
Efrem Berezin <efrem430@gmail.com>
2024-06-28 17:30:00 -0500

    Sidebar 1 part

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,12 +1,25 @@
-import React, { UseState } from 'react'
-import { Box, UseMediaQuery } from "@mui/material"
+import React, { useState } from 'react'
+import { Box, useMediaQuery } from "@mui/material"
 import { Outlet } from "react-router-dom"
 import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
 
 const Layout = () => {
-  return  <Box width="100%" height="100%">
+  const isNonMobile = useMediaQuery("(min-width: 600ps)");
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  return  <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+    <Sidebar 
+      isNonMobile={isNonMobile}
+      drawerWidth="250px"
+      isSidebarOpen={isSidebarOpen}
+      setSidebarOpen={setSidebarOpen}
+      />
     <Box>
-        <Navbar />
+        <Navbar 
+        isSidebarOpen={isSidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        />
         <Outlet />
     </Box>
   </Box>

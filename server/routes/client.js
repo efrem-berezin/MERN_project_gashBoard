b8d1f8fd0f5b9dc5b87b6516a497404d544b304e
Efrem Berezin <efrem430@gmail.com>
2024-09-23 18:25:05 -0500

    controller getTransactions

--- server/routes/client.js
+++ server/routes/client.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/products', getProducts)
 router.get('/customers', getCustomers);
-
+router.get('/transaction', getTransactions)
 
 export default router;
